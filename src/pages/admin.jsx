7bcdfde5189bf8479e259c12852e06c710b0096c
Jayanthkumar2004 <jk417236@gmail.com>
2025-08-29 12:00:15 +0530

    Update admin.jsx

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Admin.css";
+import "./admin.css";
 
 export default function Admin() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
