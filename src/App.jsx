9d17edcfaf3346d4a5d3f5148c8b50c625a515c8
Jayanthkumar2004 <jk417236@gmail.com>
2025-08-29 12:06:44 +0530

    Update App.jsx
    Fix import path: admin.jsx is lowercase

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Admin from './pages/Admin';
+import Admin from './pages/admin';
 import StockMarket from './pages/StockMarket';
 
 function App() {
